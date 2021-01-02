7422db4fde153eea1c1d51eb5cd542b048f43d19
Stephanie Raymos <stephanieraymos@gmail.com>
2021-01-02 13:32:58 -0800

    Modal js created and working on styling

---
+++ modal.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById("modal");
+
+var btn = document.getElementById("modal-btn");
+
+//Element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//Open modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+//Close modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+//Close modal if clicked off modal screen
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
